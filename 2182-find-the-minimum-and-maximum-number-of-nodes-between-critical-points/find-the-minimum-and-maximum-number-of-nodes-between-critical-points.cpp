03847c63bcd23a011b04ad0a1806d2233b4a11f3
krg <2codetogo@gmail.com>
2026-09-01 00:09:20 +0530

    Time: 17 ms (5.16%) | Memory: 138.2 MB (5.02%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,44 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-
-        vector<int> v;
-
-        // Step 1: Linked List -> Vector
-        while (head != nullptr) {
-            v.push_back(head->val);
-            head = head->next;
+        
+        vector<int>v;
+        while(head != nullptr ){
+          v.push_back(head->val);
+          head = head -> next;
         }
 
-        vector<int> critical;
-
-        // Step 2: Find critical points
-        for (int i = 1; i < v.size() - 1; i++) {
-
-            // Local maximum OR local minimum
-            if ((v[i] > v[i-1] && v[i] > v[i+1]) ||
-                (v[i] < v[i-1] && v[i] < v[i+1])) {
-
-                critical.push_back(i);
+        vector<int>c;
+        for( int i = 1 ; i < v.size()-1; i++ ){
+            if( v[i-1] < v[i] && v[i] > v[i+1] || v[i-1] > v[i] && v[i] < v[i+1] ){
+                c.push_back(i);
             }
         }
 
-        // Step 3: Fewer than 2 critical points
-        if (critical.size() < 2) {
-            return {-1, -1};
+        if( c.size() < 2 ){
+            return {-1,-1};
         }
 
-        // Step 4: Find minimum distance
-        int minDist = INT_MAX;
+        int minVal = INT_MAX;
 
-        for (int i = 1; i < critical.size(); i++) {
-            minDist = min(minDist,
-                          critical[i] - critical[i-1]);
+        for( int i = 1; i<c.size(); i++ ){
+            minVal = min( minVal , c[i] - c[i-1] );
         }
+        
+        int maxVal = c.back() - c.front();
 
-        // Step 5: Maximum distance
-        int maxDist = critical.back() - critical.front();
-
-        return {minDist, maxDist};
+        return {minVal , maxVal };
     }
 };
